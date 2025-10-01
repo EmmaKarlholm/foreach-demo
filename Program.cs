370cfa077dd4b76969b1ff46b6f9666bf65bd6f9
Emma Karlholm <emma.karlholm@gmail.com>
2025-10-01 10:40:53 +0200

    Better comment

--- Program.cs
+++ Program.cs
@@ -10,9 +10,9 @@
             {
                 Console.WriteLine($"I love {dish}!");
             }
-            // Note how short and simple that foreach loop is! It goes through all elements in foods and loops
-            // with no need of using any index (i) variables or anything like that. It applies logic using
-            // every element in an array or list.
+            // Note how short and simple that foreach loop is! It goes through all elements in foods, one at a time,
+            // and loops with no need of using any index (i) variables or anything like that.
+            // It applies logic to every element in an array or list.
         }
     }
 }
